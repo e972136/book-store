eeb80a65a14223a3cc91fefb862777eb4c0e6902
e972136 <81443673+e972136@users.noreply.github.com>
2021-10-16 10:07:51 -0600

    Add files via upload

--- src/main/java/com/gaspar/service/BookService.java
+++ src/main/java/com/gaspar/service/BookService.java
@@ -2,13 +2,16 @@ package com.gaspar.service;
 
 import com.gaspar.models.Book;
 import com.gaspar.repository.BookRepository;
+import java.lang.reflect.Field;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import org.springframework.util.ReflectionUtils;
 
 @Service
 @RequiredArgsConstructor
@@ -27,11 +30,28 @@ public class BookService {
     @Transactional
     public Book update(Integer id, Book book) {
         Book bookObtain = bookRepository.findById(id).orElseThrow(() -> new IllegalStateException("Id no existe"));
-        bookObtain.setTitle(book.getTitle());
-        return book;
+        bookObtain.set(book);
+        return bookObtain;
     }
 
     public void delete(Integer id) {
         bookRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Book patch(Integer id, Map<Object, Object> fields) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Book bookObtain = bookRepository.findById(id).orElseThrow(() -> new IllegalStateException("Id no existe"));
+        System.err.println(" encontrado: "+bookObtain);
+        if(bookObtain!=null){
+            fields.forEach((key,value)->{
+                Field findField = ReflectionUtils.findField(Book.class, key.toString());
+                findField.setAccessible(true);
+                ReflectionUtils.setField(findField, bookObtain, value);
+            });            
+            System.err.println(" actualizado: "+bookObtain);
+            
+        }
+        return bookObtain;
     }
 }
