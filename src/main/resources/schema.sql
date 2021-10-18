8222f3464471036b9f2e9556f70d72791a576b77
e972136 <81443673+e972136@users.noreply.github.com>
2021-10-18 09:30:07 -0600

    Add files via upload

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,3 +7,11 @@ create table book(
     available BOOLEAN,
     constraint pk_book_id primary key (id)
 );
+
+create table sale(
+   id integer identity not null,
+   bookId integer,
+   customerEmail varchar(50),
+   price double,
+   constraint pk_sale_id primary key (id)
+);
