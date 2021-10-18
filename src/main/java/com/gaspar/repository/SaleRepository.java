500830da651a26d2d5d8ba711d64e249f57e9be7
e972136 <81443673+e972136@users.noreply.github.com>
2021-10-18 09:28:55 -0600

    Add files via upload

---
+++ src/main/java/com/gaspar/repository/SaleRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gaspar.repository;
+
+import com.gaspar.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author ds010102
+ */
+public interface SaleRepository extends JpaRepository<Book,Integer>{
+    
+}
