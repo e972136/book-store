c86673d5be45b3c2c916497f82145b3483b0fef9
e972136 <81443673+e972136@users.noreply.github.com>
2021-10-18 15:31:00 -0600

    Add files via upload

--- src/main/java/com/gaspar/service/BookService.java
+++ src/main/java/com/gaspar/service/BookService.java
@@ -3,6 +3,7 @@ package com.gaspar.service;
 import com.gaspar.models.Book;
 import com.gaspar.repository.BookRepository;
 import java.lang.reflect.Field;
+import java.util.LinkedHashMap;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.ReflectionUtils;
 
 @Service
@@ -58,4 +61,32 @@ public class BookService {
         }
         return bookObtain;
     }
+
+    public Map<Object,Object> allBooksPage(Map<String, Object> fields) {
+       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       Integer page = 0;
+       Integer size = 2;
+       String pageStr = (String)fields.getOrDefault("page", null);
+       if(pageStr!=null){
+           page = Integer.valueOf(pageStr)-1;
+       }
+       String sizeStr = (String)fields.getOrDefault("size", null);
+       if(sizeStr!=null){
+           size = Integer.valueOf(sizeStr);
+       }
+       PageRequest pageRequest = PageRequest.of(page, size);
+       Page<Book> findAll = bookRepository.findAll(pageRequest);
+       
+       Map<Object,Object> respuesta = new LinkedHashMap<>();       
+       
+       respuesta.put("content", findAll.getContent());
+       respuesta.put("size", size);
+       respuesta.put("numberOfElements", findAll.getNumberOfElements());
+       respuesta.put("totalElements", findAll.getTotalElements());
+       respuesta.put("totalPages", findAll.getTotalPages());
+       respuesta.put("number", findAll.getNumber());
+       return respuesta;
+    }
+    
+    
 }
