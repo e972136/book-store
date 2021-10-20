52571b432a91736fc6773780047e2fff776e7201
e972136 <81443673+e972136@users.noreply.github.com>
2021-10-19 18:56:44 -0600

    Add files via upload

---
+++ src/main/java/com/gaspar/Utils.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gaspar;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author ds010102
+ */
+public class Utils {
+    public static boolean correoValido(String email){
+            Pattern pattern = Pattern
+                .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher mather = pattern.matcher(email);
+        return mather.find();
+    }
+}
